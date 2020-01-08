cdfd0190776bd02881bcf26e66739f0b09aa9fb0
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-08 21:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ class Formatter {
   static sanitize(string) {
      return string.replace(/[^A-Za-z0-9-\s']+/g, '')
   } 
- static titleize(string) {
-    let exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
-    let arr = string.split(' ')
-    let final = []
-    for ( let i = 0; i < arr.length; i++ ) {
-      if (i === 0) {
-        final.push( this.capitalize(arr[i]))
-        
-      } else {
-        
-        if ( exclude.includes(arr[i])) {
-          final.push( arr[i] )
-          
-        } else {
-          final.push( this.capitalize(arr[i]) )
-          
-        }
-      }
-    }
-    return final.join(' ')
-  }
-
-}
+ static titleize(string){
+ } 
+ }
