d489a8b99a6622ded2a929abe8e26dd9548eac58
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-08 18:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1) 
+  } 
+  static sanitize(string) {
+     return string.replace(/[^A-Za-z0-9-\s']+/g, '')
+  } 
+ static titleize(string) {
+    let exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let arr = string.split(' ')
+    let final = []
+    for ( let i = 0; i < arr.length; i++ ) {
+      if (i === 0) {
+        final.push( this.capitalize(arr[i]))
+        
+      } else {
+        
+        if ( exclude.includes(arr[i])) {
+          final.push( arr[i] )
+          
+        } else {
+          final.push( this.capitalize(arr[i]) )
+          
+        }
+      }
+    }
+    return final.join(' ')
+  }
+
 }
